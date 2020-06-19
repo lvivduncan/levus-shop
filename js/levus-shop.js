6be7d3ce66da5a11738ee5cb793b887a7c561cc8
lvivduncan <lvivduncan@gmail.com>
2020-06-19 19:38:10 +0300

    start oop

--- js/levus-shop.js
+++ js/levus-shop.js
@@ -1,236 +1,79 @@
-// basket.js 18-06-2020
-{
-	// check basket
-	if (document.querySelector('#basket')) {
+// 19-06-2020 
 
-		// wiev quantity goods
-		const quantity = document.querySelector('#basket-quantity');
+// назва сховища
+const BASKET = 'basket';
 
-		// view sum
-		const sum = document.querySelector('#basket-sum');
+// клас, що відповідає за кошик на сторінці -- загальна ціна, кількість товарів, товари тощо
+class Basket {
 
-		// view selected goods
-		const selected = document.querySelector('#basket-goods');
+  static getQuantity() {
+    return document.querySelector('#basket-quantity');
+  }
 
-		// all buttons click
-		document.querySelectorAll('button').forEach(button => button.addEventListener('click', addGoods));
+  static getSum() {
+    return document.querySelector('#basket-sum');
+  }
 
-		// clear localStorage
-		document.querySelector('#basket-clear').addEventListener('click', clearStorage);
-
-		// delete goods one by one
-		selected.addEventListener('click', e => removeGoods(e));
-
-		// view sum
-		viewSum();
-
-		// view quantity
-		viewQuantity();
-
-		// view selected goods
-		viewSelected();
-
-		// add to localStorage
-		function addGoods() {
-
-			// temporary array
-			const content = { name: this.dataset.name, size: this.dataset.size, price: this.dataset.price, number: "1", img: this.dataset.img };
-
-			// check localStorage and add data to localStorage
-			if (localStorage.getItem('basket') === null) {
-
-				// create array for data
-				const data = [];
-
-				// push object with 3 items to array
-				data.push(content);
-
-				// add data to localStorage
-				localStorage.setItem('basket', JSON.stringify(data));
-
-				// reload 
-				viewQuantity();
-				viewSum();
-				viewSelected();
-			} else {
-
-				// load data from localStorage
-				const data = JSON.parse(localStorage.getItem('basket'));
-
-				// new name 
-				const name = this.dataset.name;
-
-				if (data.find(item => item.name == name)) {
-					// checked
-					const checked = data.find(item => item.name === name);
-
-					// add 1
-					checked.number++;
-				} else {
-
-					// push object with 5 items to array
-					data.push(content);
-				}
-
-				// add data to localStorage
-				localStorage.setItem('basket', JSON.stringify(data));
-
-				// reload 
-				viewQuantity();
-				viewSum();
-				viewSelected();
-			}
-		}
-
-		// return quantity goods
-		function viewQuantity() {
-			if (localStorage.getItem('basket') === null) {
-				quantity.innerHTML = 0;
-			} else {
-				quantity.innerHTML = JSON.parse(localStorage.getItem('basket')).reduce((sum, item) => sum += +item.number, 0);
-			}
-		}
-
-		// return sum
-		function viewSum() {
-			if (localStorage.getItem('basket') === null) {
-				sum.innerHTML = 0;
-			} else {
-				sum.innerHTML = JSON.parse(localStorage.getItem('basket')).map(item => item.price * item.number).reduce((sum, item) => sum + +item, 0) + 'грн';
-			}
-		}
-
-		// clear localStorage
-		function clearStorage() {
-			localStorage.clear('basket');
-
-			// reload
-			viewSum();
-			viewQuantity();
-			viewSelected();
-		}
-
-		// view selected goods
-		function viewSelected() {
-			if (localStorage.getItem('basket') === null) {
-				selected.innerHTML = '';
-			} else {
-				selected.innerHTML =
-					JSON.parse(localStorage.getItem('basket'))
-						.reduce((sum, item, i) => sum + `<p data-id="${i}"><i></i> ${item.name} [${item.number}]: ${item.size} - ${item.price}грн</p>`, '');
-			}
-		}
-
-		// delete goods from basket
-		function removeGoods(e) {
-			if (e.target.tagName === 'I') {
-
-				// id goods
-				const id = e.target.parentNode.dataset.id;
-
-				// goods from storage
-				const data = JSON.parse(localStorage.getItem('basket'));
-
-				// remove item
-				data.splice(id, 1);
-
-				// claer storage or return data to localStorage
-				if (data.length === 0) {
-					localStorage.removeItem('basket');
-				} else {
-					localStorage.setItem('basket', JSON.stringify(data));
-				}
-
-				// reload
-				viewSum();
-				viewQuantity();
-				viewSelected();
-			}
-		}
-	}
-
-	// check #order-goods
-	if (document.querySelector('#order-goods')) {
-
-		// table
-		const order = document.querySelector('#order-goods');
-
-		const sum = document.querySelector('#order-sum');
-
-		// delete goods one by one
-		order.addEventListener('click', e => changeGoods(e));
-
-		viewGoods();
-		viewSum();
-
-		// view all ordered goods 
-		function viewGoods() {
-			if (localStorage.getItem('basket') === null) {
-				order.innerHTML = '';
-			} else {
-				order.innerHTML =
-					JSON.parse(localStorage.getItem('basket'))
-						.reduce((sum, item, i) => sum + `
-							<p data-id="${i}"><img src="${item.img}" alt=""><i></i><b>${item.name}</b><span>${item.size} </span><span><b>${item.price}</b>грн </span><span class="minus"></span><span class="number">${item.number}</span><span class="plus"></span></p>`, '');
-			}
-		}
-
-		// change quantity goods
-		function changeGoods(e) {
-
-			// id goods
-			const id = e.target.parentNode.dataset.id;
-
-			// goods from storage
-			const data = JSON.parse(localStorage.getItem('basket'));
-
-			// to delete
-			if (e.target.tagName === 'I') {
-
-				// remove item
-				data.splice(id, 1);
-
-				// claer storage or return data to localStorage
-				if (data.length === 0) {
-					localStorage.removeItem('basket');
-				} else {
-					localStorage.setItem('basket', JSON.stringify(data));
-				}
-			} else if (e.target.className === 'minus') {
-				if (data[id].number > 1) {
-					// remove 1
-					data[id].number--;
-				} else {
-					// remove item
-					data.splice(id, 1);
-				}
-			} else if (e.target.className === 'plus') {
-				// add 1
-				data[id].number++;
-			}
-
-			// return data to localStorage
-			localStorage.setItem('basket', JSON.stringify(data));
-
-			// check localStorage
-			if (localStorage.getItem('basket').length < 3) {
-				localStorage.clear('basket');
-			}
-
-			// reload
-			viewGoods();
-			viewSum();
-		}
-
-		// return sum
-		function viewSum() {
-			if (localStorage.getItem('basket') === null) {
-				sum.style.display = 'none';
-			} else {
-				sum.innerHTML = JSON.parse(localStorage.getItem('basket')).map(item => item.price * item.number).reduce((sum, item) => sum + +item, 0) + 'грн';
-				sum.style.display = 'block';
-			}
-		}
-
-	}
+  static getGoods() {
+    return document.querySelector('#basket-goods');
+  }
 }
+
+// клас, який опрацьовує товари на сторінці
+class Items {
+  static getItems() {
+    return document.querySelectorAll('.button');
+  }
+
+  static toBasket() {
+    // дані з дата-атрибутів
+    const content = { name: this.dataset.name, size: this.dataset.size, price: this.dataset.price, number: "1", img: this.dataset.img };
+
+    // якщо дані вже у сховищі є, тоді додаємо до них. або ж надсилаємо перші
+    if (Storage.has()) {
+      Storage.add(content);
+    } else {
+      Storage.set(content);
+    }
+  }
+}
+
+// localStorage
+class Storage {
+  // перевірка чи існує сховище
+  static has() {
+    if (localStorage.getItem(BASKET) !== null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // отримуємо дані
+  static get() {
+    return JSON.parse(localStorage.getItem(BASKET));
+  }
+
+  // надсилаємо дані
+  static set(value) {
+    // масив для сховища
+    const data = [];
+    // додали дані
+    data.push(value);
+    // надсилаємо у сховище
+    localStorage.setItem(BASKET, JSON.stringify(data));
+  }
+
+  // додаємо дані
+  static add(value) {
+    // масив зі сховища
+    const data = JSON.parse(localStorage.getItem(BASKET));
+    // додали дані
+    data.push(value);
+    // повертаємо у сховище оновлений масив
+    localStorage.setItem(BASKET, JSON.stringify(data));
+  }
+}
+
+// клік на кнопці "button"
+Items.getItems().forEach(item => item.addEventListener('click', Items.toBasket));
