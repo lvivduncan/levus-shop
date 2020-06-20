4df6111339db2e330da54b7b448003dc4d453e79
lvivduncan <lvivduncan@gmail.com>
2020-06-20 15:41:39 +0300

    add ie5

--- js/levus-shop.ie5.js
+++ js/levus-shop.ie5.js
@@ -1,232 +1,314 @@
-// basket.js 18-06-2020
+// 20-06-2020 
 "use strict";
-{
-  // check basket
-  if (document.querySelector('#basket')) {
-    // add to localStorage
-    var addGoods = function addGoods() {
-      // temporary array
+
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+// назва сховища
+var BASKET = 'basket'; // клас, що відповідає за кошик на сторінці -- загальна ціна, кількість товарів, товари тощо
+
+var Basket = /*#__PURE__*/function () {
+  function Basket() {
+    _classCallCheck(this, Basket);
+  }
+
+  _createClass(Basket, null, [{
+    key: "getQuantity",
+    value: function getQuantity() {
+      return document.querySelector('#basket-quantity');
+    }
+  }, {
+    key: "getSum",
+    value: function getSum() {
+      return document.querySelector('#basket-sum');
+    }
+  }, {
+    key: "getGoods",
+    value: function getGoods() {
+      return document.querySelector('#basket-goods');
+    }
+  }, {
+    key: "getClear",
+    value: function getClear() {
+      return document.querySelector('#basket-clear');
+    } // опрацьовуємо дані
+
+  }, {
+    key: "viewQuantity",
+    value: function viewQuantity() {
+      return Storage.get().reduce(function (sum, item) {
+        return sum += +item.number;
+      }, 0);
+    }
+  }, {
+    key: "viewSum",
+    value: function viewSum() {
+      return Storage.get().reduce(function (sum, item) {
+        return sum += item.price * item.number;
+      }, 0);
+    }
+  }, {
+    key: "viewGoods",
+    value: function viewGoods() {
+      return Storage.get().reduce(function (sum, item, i) {
+        return sum += "<p data-id=\"".concat(i, "\"><i></i><b>").concat(item.name, "</b>, ").concat(item.size, ": ").concat(item.price, ", ").concat(item.number, "</p>");
+      }, '');
+    } // видаляємо товари
+
+  }, {
+    key: "removeGoods",
+    value: function removeGoods(e) {
+      if (e.target.tagName === 'I') {
+        var id = e.target.parentNode.dataset.id;
+        Storage.remove(id);
+        Basket.reload();
+        Checkout.reload();
+      }
+    } // очистка кошика
+
+  }, {
+    key: "clearBasket",
+    value: function clearBasket() {
+      Storage.clear();
+      Basket.getQuantity().innerHTML = 0;
+      Basket.getSum().innerHTML = 0;
+      Basket.getGoods().innerHTML = '';
+    } // 1 метод, який оновлює дані у кошикові
+
+  }, {
+    key: "reload",
+    value: function reload() {
+      // перевірка чи існує кошик =)
+      if (Basket.getQuantity()) {
+        if (Storage.has()) {
+          Basket.getQuantity().innerHTML = Basket.viewQuantity();
+          Basket.getSum().innerHTML = Basket.viewSum();
+          Basket.getGoods().innerHTML = Basket.viewGoods();
+        } else {
+          Basket.getQuantity().innerHTML = 0;
+          Basket.getSum().innerHTML = 0;
+          Basket.getGoods().innerHTML = '';
+        }
+      }
+    }
+  }]);
+
+  return Basket;
+}(); // клас, який опрацьовує товари на сторінці
+
+
+var Items = /*#__PURE__*/function () {
+  function Items() {
+    _classCallCheck(this, Items);
+  }
+
+  _createClass(Items, null, [{
+    key: "getItems",
+    value: function getItems() {
+      return document.querySelectorAll('.button');
+    }
+  }, {
+    key: "toBasket",
+    value: function toBasket() {
+      // дані з дата-атрибутів
       var content = {
         name: this.dataset.name,
         size: this.dataset.size,
         price: this.dataset.price,
         number: "1",
         img: this.dataset.img
-      }; // check localStorage and add data to localStorage
+      }; // якщо дані вже у сховищі є, тоді додаємо до них. або ж надсилаємо перші
 
-      if (localStorage.getItem('basket') === null) {
-        // create array for data
-        var data = []; // push object with 3 items to array
-
-        data.push(content); // add data to localStorage
-
-        localStorage.setItem('basket', JSON.stringify(data)); // reload 
-
-        viewQuantity();
-        viewSum();
-        viewSelected();
+      if (Storage.has()) {
+        Storage.add(content);
       } else {
-        // load data from localStorage
-        var _data = JSON.parse(localStorage.getItem('basket')); // new name 
-
-
-        var name = this.dataset.name;
-
-        if (_data.find(function (item) {
-          return item.name == name;
-        })) {
-          // checked
-          var checked = _data.find(function (item) {
-            return item.name === name;
-          }); // add 1
-
-
-          checked.number++;
-        } else {
-          // push object with 5 items to array
-          _data.push(content);
-        } // add data to localStorage
-
-
-        localStorage.setItem('basket', JSON.stringify(_data)); // reload 
-
-        viewQuantity();
-        viewSum();
-        viewSelected();
+        Storage.set(content);
       }
-    }; // return quantity goods
+
+      Basket.reload();
+    }
+  }]);
+
+  return Items;
+}(); // localStorage
 
 
-    var viewQuantity = function viewQuantity() {
-      if (localStorage.getItem('basket') === null) {
-        quantity.innerHTML = 0;
+var Storage = /*#__PURE__*/function () {
+  function Storage() {
+    _classCallCheck(this, Storage);
+  }
+
+  _createClass(Storage, null, [{
+    key: "has",
+    // перевірка чи існує сховище
+    value: function has() {
+      if (localStorage.getItem(BASKET) !== null) {
+        return true;
       } else {
-        quantity.innerHTML = JSON.parse(localStorage.getItem('basket')).reduce(function (sum, item) {
-          return sum += +item.number;
+        return false;
+      }
+    } // отримуємо дані
+
+  }, {
+    key: "get",
+    value: function get() {
+      return JSON.parse(localStorage.getItem(BASKET));
+    } // надсилаємо дані
+
+  }, {
+    key: "set",
+    value: function set(value) {
+      if (Array.isArray(value)) {
+        localStorage.setItem(BASKET, JSON.stringify(value));
+      } else {
+        var data = [];
+        data.push(value);
+        localStorage.setItem(BASKET, JSON.stringify(data));
+      }
+    } // додаємо дані
+
+  }, {
+    key: "add",
+    value: function add(value) {
+      // масив зі сховища
+      var data = JSON.parse(localStorage.getItem(BASKET)); // якщо такий товар уже є, то збільшуємо кількість на 1. або ж просто додаємо новий товар
+
+      if (data.find(function (item) {
+        return item.name === value.name;
+      })) {
+        var checked = data.find(function (item) {
+          return item.name === value.name;
+        });
+        checked.number++;
+      } else {
+        // додали дані
+        data.push(value);
+      } // повертаємо у сховище оновлений масив
+
+
+      localStorage.setItem(BASKET, JSON.stringify(data));
+    } // очистка сховища
+
+  }, {
+    key: "clear",
+    value: function clear() {
+      localStorage.clear(BASKET);
+    } // видаляємо товар
+
+  }, {
+    key: "remove",
+    value: function remove(id) {
+      var data = JSON.parse(localStorage.getItem(BASKET));
+      data.splice(id, 1);
+
+      if (data.length === 0) {
+        localStorage.removeItem(BASKET);
+      } else {
+        localStorage.setItem(BASKET, JSON.stringify(data));
+      }
+    }
+  }]);
+
+  return Storage;
+}(); // оформлення замовлення
+
+
+var Checkout = /*#__PURE__*/function () {
+  function Checkout() {
+    _classCallCheck(this, Checkout);
+  }
+
+  _createClass(Checkout, null, [{
+    key: "getOrderGoods",
+    value: function getOrderGoods() {
+      return document.querySelector('#order-goods');
+    }
+  }, {
+    key: "getOrderSum",
+    value: function getOrderSum() {
+      return document.querySelector('#order-sum');
+    } // виводимо дані
+
+  }, {
+    key: "viewGoods",
+    value: function viewGoods() {
+      if (Storage.has()) {
+        var data = Storage.get().reduce(function (sum, item, i) {
+          return sum + "<div data-id=\"".concat(i, "\" class=\"product\"><div class=\"product-img\"><img src=\"").concat(item.img, "\" alt=\"\"></div><div class=\"product-name\">").concat(item.name, "</div><div class=\"product-size\">").concat(item.size, "</div><div class=\"product-price\"><small>").concat(item.price, "\u0433\u0440\u043D</small> <p>").concat(item.price * item.number, "\u0433\u0440\u043D</p></div><div class=\"product-quantity\"><span class=\"minus\"></span><span class=\"number\">").concat(item.number, "</span><span class=\"plus\"></span></div><div class=\"product-delete\"><i></i></div></div>");
+        }, ''); // todo: delete 'грн'
+
+        return data;
+      }
+    }
+  }, {
+    key: "viewSum",
+    value: function viewSum() {
+      if (Storage.has()) {
+        return Storage.get().map(function (item) {
+          return item.price * item.number;
+        }).reduce(function (sum, item) {
+          return sum + +item;
         }, 0);
       }
-    }; // return sum
+    } // метод для роботи з оформленням товарів: видалення і зміна кілкості
 
-
-    var viewSum = function viewSum() {
-      if (localStorage.getItem('basket') === null) {
-        sum.innerHTML = 0;
-      } else {
-        sum.innerHTML = JSON.parse(localStorage.getItem('basket')).map(function (item) {
-          return item.price * item.number;
-        }).reduce(function (sum, item) {
-          return sum + +item;
-        }, 0) + 'грн';
-      }
-    }; // clear localStorage
-
-
-    var clearStorage = function clearStorage() {
-      localStorage.clear('basket'); // reload
-
-      viewSum();
-      viewQuantity();
-      viewSelected();
-    }; // view selected goods
-
-
-    var viewSelected = function viewSelected() {
-      if (localStorage.getItem('basket') === null) {
-        selected.innerHTML = '';
-      } else {
-        selected.innerHTML = JSON.parse(localStorage.getItem('basket')).reduce(function (sum, item, i) {
-          return sum + "<p data-id=\"".concat(i, "\"><i></i> ").concat(item.name, " [").concat(item.number, "]: ").concat(item.size, " - ").concat(item.price, "\u0433\u0440\u043D</p>");
-        }, '');
-      }
-    }; // delete goods from basket
-
-
-    var removeGoods = function removeGoods(e) {
-      if (e.target.tagName === 'I') {
-        // id goods
-        var id = e.target.parentNode.dataset.id; // goods from storage
-
-        var data = JSON.parse(localStorage.getItem('basket')); // remove item
-
-        data.splice(id, 1); // claer storage or return data to localStorage
-
-        if (data.length === 0) {
-          localStorage.removeItem('basket');
-        } else {
-          localStorage.setItem('basket', JSON.stringify(data));
-        } // reload
-
-
-        viewSum();
-        viewQuantity();
-        viewSelected();
-      }
-    };
-
-    // wiev quantity goods
-    var quantity = document.querySelector('#basket-quantity'); // view sum
-
-    var sum = document.querySelector('#basket-sum'); // view selected goods
-
-    var selected = document.querySelector('#basket-goods'); // all buttons click
-
-    document.querySelectorAll('button').forEach(function (button) {
-      return button.addEventListener('click', addGoods);
-    }); // clear localStorage
-
-    document.querySelector('#basket-clear').addEventListener('click', clearStorage); // delete goods one by one
-
-    selected.addEventListener('click', function (e) {
-      return removeGoods(e);
-    }); // view sum
-
-    viewSum(); // view quantity
-
-    viewQuantity(); // view selected goods
-
-    viewSelected();
-  } // check #order-goods
-
-
-  if (document.querySelector('#order-goods')) {
-    // view all ordered goods 
-    var viewGoods = function viewGoods() {
-      if (localStorage.getItem('basket') === null) {
-        order.innerHTML = '';
-      } else {
-        order.innerHTML = JSON.parse(localStorage.getItem('basket')).reduce(function (sum, item, i) {
-          return sum + "\n\t\t\t\t\t\t\t<p data-id=\"".concat(i, "\"><img src=\"").concat(item.img, "\" alt=\"\"><i></i><b>").concat(item.name, "</b><span>").concat(item.size, " </span><span><b>").concat(item.price, "</b>\u0433\u0440\u043D </span><span class=\"minus\"></span><span class=\"number\">").concat(item.number, "</span><span class=\"plus\"></span></p>");
-        }, '');
-      }
-    }; // change quantity goods
-
-
-    var changeGoods = function changeGoods(e) {
-      // id goods
-      var id = e.target.parentNode.dataset.id; // goods from storage
-
-      var data = JSON.parse(localStorage.getItem('basket')); // to delete
+  }, {
+    key: "changeGoods",
+    value: function changeGoods(e) {
+      var id = e.target.parentNode.parentNode.dataset.id;
+      var data = Storage.get();
 
       if (e.target.tagName === 'I') {
-        // remove item
-        data.splice(id, 1); // claer storage or return data to localStorage
-
-        if (data.length === 0) {
-          localStorage.removeItem('basket');
-        } else {
-          localStorage.setItem('basket', JSON.stringify(data));
-        }
+        data.splice(id, 1);
+        data.length === 0 ? Storage.clear() : Storage.set(data);
       } else if (e.target.className === 'minus') {
-        if (data[id].number > 1) {
-          // remove 1
-          data[id].number--;
-        } else {
-          // remove item
-          data.splice(id, 1);
-        }
+        data[id].number > 1 ? data[id].number-- : data.splice(id, 1);
       } else if (e.target.className === 'plus') {
-        // add 1
         data[id].number++;
-      } // return data to localStorage
-
-
-      localStorage.setItem('basket', JSON.stringify(data)); // check localStorage
-
-      if (localStorage.getItem('basket').length < 3) {
-        localStorage.clear('basket');
-      } // reload
-
-
-      viewGoods();
-
-      _viewSum();
-    }; // return sum
-
-
-    var _viewSum = function _viewSum() {
-      if (localStorage.getItem('basket') === null) {
-        _sum.style.display = 'none';
-      } else {
-        _sum.innerHTML = JSON.parse(localStorage.getItem('basket')).map(function (item) {
-          return item.price * item.number;
-        }).reduce(function (sum, item) {
-          return sum + +item;
-        }, 0) + 'грн';
-        _sum.style.display = 'block';
       }
-    };
 
-    // table
-    var order = document.querySelector('#order-goods');
+      Storage.set(data);
+      Checkout.reload();
+      Basket.reload();
+    } // 1 метод, який оновлює дані 
 
-    var _sum = document.querySelector('#order-sum'); // delete goods one by one
+  }, {
+    key: "reload",
+    value: function reload() {
+      // якщо не на сторінці оформлення, щоб не було помилки
+      if (Checkout.getOrderGoods() !== null) {
+        if (Storage.has()) {
+          Checkout.getOrderGoods().innerHTML = Checkout.viewGoods();
+          Checkout.getOrderSum().innerHTML = Checkout.viewSum();
+        } else {
+          Checkout.getOrderGoods().innerHTML = '';
+          Checkout.getOrderSum().innerHTML = '';
+        }
+      }
+    }
+  }]);
+
+  return Checkout;
+}(); // клік на кнопці "button"
 
 
-    order.addEventListener('click', function (e) {
-      return changeGoods(e);
-    });
-    viewGoods();
+Items.getItems().forEach(function (item) {
+  return item.addEventListener('click', Items.toBasket);
+}); // очистити кошик, якщо кнопка очистки існує =)
 
-    _viewSum();
-  }
-}
+Basket.getClear() && Basket.getClear().addEventListener('click', Basket.clearBasket); // оновлюємо кошик при завантаженні сторінки
+
+Basket.reload(); // видаляємо товари з кошика
+
+Basket.getGoods() && Basket.getGoods().addEventListener('click', Basket.removeGoods); // сторінка оформлення замволення
+
+Checkout.reload(); // видаляємо/змінюємо кількість
+
+Checkout.getOrderGoods().addEventListener('click', function (e) {
+  return Checkout.changeGoods(e);
+});
